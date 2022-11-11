bf4cce95a3c154a0ed176509306992e39344d501
Seti Ngabo <seti.ngabo@mths.ca>
2022-11-11 14:41:33 +0000

    init

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@
 
 import promptSync from 'prompt-sync'
 
+/**
+ * The function prints the original string replaced by
+ * that many occurrences of the digit to the right of the string
+ *
+ * @param {string} string from the user
+ * @returns {string} string that blowup numbers to letters
+ */
+
 // constants
 const prompt = promptSync()
 
@@ -14,39 +22,53 @@ const prompt = promptSync()
 const userInput = prompt('Enter any string with numbers: ')
 
 // Proccess
-const logic1 = userInput.split('')
-const logic2 = []
+function blowUp(string: string): string {
+  let logic = []
+  const finalLogic = []
+  let firstString: string
+  let secondString: string
+  let firstInt: number = -1
+  let secondInt: number = -1
+  logic = Array.from(string)
 
-// loop counter
-for (let count = 0; count < logic1.length; count++) {
-  const string = logic1[count]
-  let newString = ''
-  if (isNaN(Number(string))) {
-    if (count - 1 !== -1) {
-      if (isNaN(Number(logic1[count - 1]))) {
-        newString = string
-        logic2.push(newString)
+  // loop counter
+  for (let count = 0; count < logic.length; count++) {
+    firstString = logic[count]
+    firstInt = parseInt(firstString)
+    if (count + 1 != logic.length) {
+            secondString = logic[count +1]
+            secondInt = parseInt(secondString)
+    if (isNaN(firstInt)) {
+            finalLogic.push(firstString)
+            continue
+    } else if (Number.isInteger(firstInt) && Number.isInteger(secondInt)) {
+            continue
+        } else {
+                for (let i = 0; i < firstInt; i++) {
+                        finalLogic.push(secondString)
       }
+      continue
+    }
+    // if the secondString
     } else {
-      newString = string
-      logic2.push(newString)
+        if (isNaN(firstInt)) {
+                  finalLogic.push(firstString)
+          } else {
+                  continue
+          }
     }
-  } else {
-    if (logic1[count + 1] !== undefined) {
-      for (let count2 = 0; count2 < Number(string); count2++) {
-        newString = newString.concat(logic1[count + 1])
-      }
-    }
-    if (isNaN(Number(logic1[count + 1]))) {
-      count = count + 1
-    }
-
-    logic2.push(newString)
   }
+    
+  const finalOutput = finalLogic.join('')
+  return finalOutput
 }
 
-// Output
-const finalOutput = logic2.join('')
-console.log(finalOutput)
-
+ // output
+let newString: string
+if (userInput !== '') {
+        newString = blowUp(userInput)
+        console.log(newString)
+} else {
+        console.log('\nInsert a string with a number.')
+}
 console.log('\nDone.')
